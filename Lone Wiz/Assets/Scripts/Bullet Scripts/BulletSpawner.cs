ed814b06cd7c9fe263189cc8badf148824b0867c
RubertDaBoi <rjpapa401@gmail.com>
2025-02-15 21:30:45 -0500

    Shotgun
    Shotgun shot type implemented fully

--- Lone Wiz/Assets/Scripts/Bullet Scripts/BulletSpawner.cs
+++ Lone Wiz/Assets/Scripts/Bullet Scripts/BulletSpawner.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    enum SpawnerType { Straight, Spin }
+    enum SpawnerType { Straight, Spin, Shotgun }
 
     [Header("Bullet Attributes")]
     public GameObject bullet;
     public float bulletLife = 1f;
     public float speed = 1f;
+    public int shotgunPellets = 5;
+    public float spreadAngle = 15f;
 
     [Header("Spawner Attributes")]
     [SerializeField] private SpawnerType spawnerType;
     [SerializeField] private float firingRate = 1f;
 
-    private GameObject spawnedBullet;
     private float timer = 0f;
-
-    private void Start()
-    {
-
-
-    }
 
     void Update()
     {
         timer += Time.deltaTime;
-        if (spawnerType == SpawnerType.Spin) transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z + 1f);
-        if(timer >= firingRate)
+        if (spawnerType == SpawnerType.Spin)
+            transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z + 1f);
+
+        if (timer >= firingRate)
         {
             Fire();
             timer = 0;
@@ -39,10 +36,26 @@ public class BulletSpawner : MonoBehaviour
     {
         if (bullet)
         {
-            spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            spawnedBullet.GetComponent<Bullet>().speed = speed;
-            spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
-            spawnedBullet.transform.rotation = transform.rotation;
+            if (spawnerType == SpawnerType.Shotgun)
+            {
+                float startAngle = -spreadAngle * (shotgunPellets - 1) / 2;
+                for (int i = 0; i < shotgunPellets; i++)
+                {
+                    float angle = startAngle + (spreadAngle * i);
+                    Quaternion rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + angle);
+                    GameObject spawnedBullet = Instantiate(bullet, transform.position, rotation);
+                    Bullet bulletScript = spawnedBullet.GetComponent<Bullet>();
+                    bulletScript.speed = speed;
+                    bulletScript.bulletLife = bulletLife;
+                }
+            }
+            else
+            {
+                GameObject spawnedBullet = Instantiate(bullet, transform.position, transform.rotation);
+                Bullet bulletScript = spawnedBullet.GetComponent<Bullet>();
+                bulletScript.speed = speed;
+                bulletScript.bulletLife = bulletLife;
+            }
         }
     }
 }
