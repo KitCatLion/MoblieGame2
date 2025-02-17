7f41e8106c7cbe0f58d262b072ea65ddcd13b70d
Chancey41 <chanceboecker@gmail.com>
2025-02-17 14:12:23 -0500

    Updated upgrade scripts

--- Lone Wiz/Assets/Scripts_Boecker/Ability.cs
+++ Lone Wiz/Assets/Scripts_Boecker/Ability.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Ability : MonoBehaviour
 {
     private string Name;
-    private float Level, Dps, Range, ProjectileNum, Speed;
+    private float Level, Dps, Range, Amount, Speed;
 
     /// <summary>
     /// An ability is upgrade by the user after death and evolve in strengh 
@@ -21,6 +21,28 @@ public class Ability : MonoBehaviour
     }
 
     //get/set name, lvl, dps, range, projectile#, speed...
-    
-    
+    public int level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    public float dps
+    {
+        get { return dps; }
+        set { dps = value; }
+    }
+
+    public float amount
+    {
+        get { return amount; }
+        set { amount = value; }
+    }
+
+    public float speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
 }
--- Lone Wiz/Assets/Scripts_Boecker/UpgradeSystem.cs
+++ Lone Wiz/Assets/Scripts_Boecker/UpgradeSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Security.Cryptography;
 using UnityEngine;
 //using UnityEngine.Windows;
 
@@ -14,7 +15,7 @@ public class UpgradeSystem : MonoBehaviour
     public string readFileName;
     public string readDirectory;
 
-    Ability A1, A2, A3, A4;
+    Ability Fireball, IceShard, Lightning;
 
     #region Updrage Txt
     void WriteToFile(string file = "")
@@ -31,34 +32,34 @@ public class UpgradeSystem : MonoBehaviour
 
         //Problem? Reuse streamwriter?
         /*using(StreamWriter alt_sw = new StreamWriter(new FileStream(sDirectory + @"\Alt_" + sFileName, FileMode.CreateNew)))
-		//using(StreamWriter alt_sw = new StreamWriter(sDirectory + @"\Alt_" + sFileName))
-		{
-			for(int i = 0; i < stat.ScreenItemsName.Length; i++){
-				alt_sw.WriteLine(stat.ScreenItemsName[i] + ": " + stat.ScreenItemsValue[i].ToString());
-				//alt_sw.WriteLine(transform.position.x.ToString());
-				//alt_sw.WriteLine(transform.position.y.ToString());
-				//alt_sw.WriteLine(transform.position.z.ToString());
-			}
-		}
-		 */
+  //using(StreamWriter alt_sw = new StreamWriter(sDirectory + @"\Alt_" + sFileName))
+  {
+      for(int i = 0; i < stat.ScreenItemsName.Length; i++){
+          alt_sw.WriteLine(stat.ScreenItemsName[i] + ": " + stat.ScreenItemsValue[i].ToString());
+          //alt_sw.WriteLine(transform.position.x.ToString());
+          //alt_sw.WriteLine(transform.position.y.ToString());
+          //alt_sw.WriteLine(transform.position.z.ToString());
+      }
+  }
+   */
 
 
         fs = new FileStream(uDirectory + @"\" + UpgradeTxt, FileMode.Create);
 
         /*
-		if(File.Exists(sDirectory + @"\" + sFileName))
-		{
-			DeleteFile(sFileName);
-		}
-		*/
+  if(File.Exists(sDirectory + @"\" + sFileName))
+  {
+      DeleteFile(sFileName);
+  }
+  */
 
         sw = new StreamWriter(fs);
-        
+
 
         sw.Flush();
         sw.Close();
     }
-    void ReadFile(string file = "")
+    void ReadFile(string ability, int level, string file = "")
     {
         print("Reading Flat");
         if (file != "")
@@ -69,21 +70,66 @@ public class UpgradeSystem : MonoBehaviour
 
         {
             //sr.readline()
-            
+
         }
 
     }
     #endregion
+    #region Upgrades
+    float[] fireDPS = { 1, 2, 3 };
+    float[] iceDPS = { 1, 2, 3 };
+    float[] lightingDPS = { 1, 2, 3 };
+    #endregion
+
+    int New_Level;
+    float New_Dps, New_Amount, New_Speed;
     void Start()
-    {
-        UpgradeTxt = "Ability_Txt";
-        
-        A1 = new Ability("Fire Ball", 1); //Example
+    {        
+        Fireball = new Ability("Fireball", 0);//Example
+        upgrade_Fire();
+
+        IceShard = new Ability("Ice Shard", 0);
+        upgrade_Ice();
+
+        Lightning = new Ability("Lightning", 0);
+        upgrade_Lightning();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //reset temps
+        New_Level = 0;
+        New_Dps = 0;
+        New_Amount = 0;
+        New_Speed = 0;
+    }
+
+    public void upgrade_Fire()  //use buttons
+    {
+        int level = Fireball.level++;
+        ReadFile("Fireball", level, readFileName);
+        Fireball.level = New_Level;
+        Fireball.dps = New_Dps;
+        Fireball.amount = New_Amount;
+        Fireball.speed = New_Speed;
+    }
+    public void upgrade_Ice()
+    {
+        int level = IceShard.level++;
+        ReadFile("Ice Shard", level, readFileName);
+        IceShard.level = New_Level;
+        IceShard.dps = New_Dps;
+        IceShard.amount = New_Amount;
+        IceShard.speed = New_Speed;
+    }
+    public void upgrade_Lightning()
+    {
+        int level = Lightning.level++;
+        ReadFile("Lightning", level, readFileName);
+        Lightning.level = New_Level;
+        Lightning.dps = New_Dps;
+        Lightning.amount = New_Amount;
+        Lightning.speed = New_Speed;
     }
 }
