85a57333b34e0000f0ca7190c173ed85ea5867d6
Chancey41 <chanceboecker@gmail.com>
2025-02-19 18:28:21 -0500

    upt

--- Lone Wiz/Assets/Scripts_Boecker/UpgradeSystem.cs
+++ Lone Wiz/Assets/Scripts_Boecker/UpgradeSystem.cs
@@ -6,81 +6,81 @@ using UnityEngine;
 
 public class UpgradeSystem : MonoBehaviour
 {
-    FileStream fs = null;
-    StreamWriter sw = null;
+    //FileStream fs = null;
+    //StreamWriter sw = null;
 
-    public string UpgradeTxt;
-    public string uDirectory;
+    //public string UpgradeTxt;
+    //public string uDirectory;
 
-    public string readFileName;
-    public string readDirectory;
+    //public string readFileName;
+    //public string readDirectory;
 
     Ability Fireball, IceShard, Lightning, Shield; //add shield
 
-    #region Updrage Txt
-    void WriteToFile(string file = "")
-    {
-        print("received message write flat");
-        if (file != "")
-            UpgradeTxt = file;
+    #region Updrage Txtfile
+  //  void WriteToFile(string file = "")
+  //  {
+  //      print("received message write flat");
+  //      if (file != "")
+  //          UpgradeTxt = file;
 
-        if (!Directory.Exists(uDirectory))
-        {
-            Directory.CreateDirectory(uDirectory);
-            Debug.Log("A folder called " + uDirectory + " has been created.");
-        }
+  //      if (!Directory.Exists(uDirectory))
+  //      {
+  //          Directory.CreateDirectory(uDirectory);
+  //          Debug.Log("A folder called " + uDirectory + " has been created.");
+  //      }
 
-        //Problem? Reuse streamwriter?
-        /*using(StreamWriter alt_sw = new StreamWriter(new FileStream(sDirectory + @"\Alt_" + sFileName, FileMode.CreateNew)))
-  //using(StreamWriter alt_sw = new StreamWriter(sDirectory + @"\Alt_" + sFileName))
-  {
-      for(int i = 0; i < stat.ScreenItemsName.Length; i++){
-          alt_sw.WriteLine(stat.ScreenItemsName[i] + ": " + stat.ScreenItemsValue[i].ToString());
-          //alt_sw.WriteLine(transform.position.x.ToString());
-          //alt_sw.WriteLine(transform.position.y.ToString());
-          //alt_sw.WriteLine(transform.position.z.ToString());
-      }
-  }
-   */
-
-
-        fs = new FileStream(uDirectory + @"\" + UpgradeTxt, FileMode.Create);
-
-        /*
-  if(File.Exists(sDirectory + @"\" + sFileName))
-  {
-      DeleteFile(sFileName);
-  }
-  */
-
-        sw = new StreamWriter(fs);
+  //      //Problem? Reuse streamwriter?
+  //      /*using(StreamWriter alt_sw = new StreamWriter(new FileStream(sDirectory + @"\Alt_" + sFileName, FileMode.CreateNew)))
+  ////using(StreamWriter alt_sw = new StreamWriter(sDirectory + @"\Alt_" + sFileName))
+  //{
+  //    for(int i = 0; i < stat.ScreenItemsName.Length; i++){
+  //        alt_sw.WriteLine(stat.ScreenItemsName[i] + ": " + stat.ScreenItemsValue[i].ToString());
+  //        //alt_sw.WriteLine(transform.position.x.ToString());
+  //        //alt_sw.WriteLine(transform.position.y.ToString());
+  //        //alt_sw.WriteLine(transform.position.z.ToString());
+  //    }
+  //}
+  // */
 
 
-        sw.Flush();
-        sw.Close();
-    }
-    void ReadFile(string ability, int level, string file = "")
-    {
-        print("Reading Flat");
-        if (file != "")
-            readFileName = file;
+  //      fs = new FileStream(uDirectory + @"\" + UpgradeTxt, FileMode.Create);
+
+  //      /*
+  //if(File.Exists(sDirectory + @"\" + sFileName))
+  //{
+  //    DeleteFile(sFileName);
+  //}
+  //*/
+
+  //      sw = new StreamWriter(fs);
 
 
-        using (StreamReader sr = new StreamReader(readDirectory + @"\" + readFileName))
+  //      sw.Flush();
+  //      sw.Close();
+  //  }
+  //  void ReadFile(string ability, int level, string file = "")
+  //  {
+  //      print("Reading Flat");
+  //      if (file != "")
+  //          readFileName = file;
 
-        {
-            //sr.readline() // read name, >, correct , , close <
-            while(sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                if(line == ability)
-                {
+
+  //      using (StreamReader sr = new StreamReader(readDirectory + @"\" + readFileName))
+
+  //      {
+  //          //sr.readline() // read name, >, correct , , close <
+  //          while(sr.EndOfStream)
+  //          {
+  //              string line = sr.ReadLine();
+  //              if(line == ability)
+  //              {
                     
-                }
-            }
-        }
+  //              }
+  //          }
+  //      }
 
-    }
+  //  }
     #endregion
 
     #region Upgrades
