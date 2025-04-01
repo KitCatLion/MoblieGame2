960f7fc5664ac233f4d2a7531c7f642d3331ca00
KitCatLion <91389279+KitCatLion@users.noreply.github.com>
2025-03-31 20:18:46 -0400

    Hearts

--- Lone Wiz/Assets/Scripts/PlayerMovement.cs
+++ Lone Wiz/Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,21 @@ public class PlayerMovement : MonoBehaviour
     public FloatingJoystick Joystick;
     public float playerSpeed;
     private Rigidbody2D rb;
-    public int hp;
+    public float hp;
+    private int HpNum;
+    public GameObject[] Hearts = new GameObject[3];
+    public Sprite[] HeartQuarts = new Sprite[3];
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        hp = 5;
+        HpNum = 3;
+        hp = 1;
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
         CurrentHP();
+        WhichHeart();
     }
     private void FixedUpdate()
     {
@@ -40,9 +45,44 @@ public class PlayerMovement : MonoBehaviour
     }
     public void CurrentHP()
     {
-        if (hp <= 0)
+        if (HpNum <= 0)
         {
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    public void HeartDecrease(GameObject currentHeart)
+    {
+        if(hp >= .75 && hp < 1)
+        {
+            currentHeart.GetComponent<SpriteRenderer>().sprite = HeartQuarts[0];
+        }
+        else if (hp >= .50 && hp < .75)
+        {
+            currentHeart.GetComponent<SpriteRenderer>().sprite = HeartQuarts[1];
+        }
+        else if(hp >= .25 && hp < .50)
+        {
+            currentHeart.GetComponent<SpriteRenderer>().sprite = HeartQuarts[2];
+        }
+        else if(hp < .25)
+        {
+            currentHeart.active = false;
+            HpNum--;
+        }
+    }
+    public void WhichHeart()
+    {
+        if (HpNum == 3)
+        {
+            HeartDecrease(Hearts[2]);
+        }
+        else if (HpNum == 2) 
+        {
+            HeartDecrease(Hearts[1]);
+        }
+        else if(HpNum == 1)
+        {
+            HeartDecrease(Hearts[0]);
         }
     }
 }
